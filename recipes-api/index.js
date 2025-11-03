4b288ea4cdb44d30e03f7ee874961b5ff01d7316
nicolelinh444 <nicolelinh444@gmail.com>
2025-11-03 12:58:40 -0800

    completed index.js for recipes-api

--- recipes-api/index.js
+++ recipes-api/index.js
@@ -2,26 +2,93 @@
 // Boilerplate Code to Set Up Server
 // ---------------------------------
 
+//Importing all of our node modules
+import express from "express"; // the framework that lets us build webservers
+import fs from "fs/promises"; // the file system module lets us read and write data from files
+
+//Declare a variable named app and call the express() function to create a new instance of express so we can use all of the methods, fucntions, properties of express
+// which will be saved in app
+const app = express();
+
+//Defining out port number
+//What port should our server listen to?
+const port = 3000; // you can use any port # but developers commonly use 3000. also there are some port numbers you cannot use
+
+//Declaring that this server will be receiving and responding to requests in JSON
+app.use(express.json());
+
+//Turn on our server so that it can listen for requests and respond to those requests at our port #
+//Hello you are on , listen to requests and respond to those requests
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+}); //this method is turning on our server
+
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
 
 // 1. getAllRecipes()
+async function getAllRecipes() {
+  const data = await fs.readFile("./recipes-data.json", "utf8");
+  const parsedRecipes = JSON.parse(data);
+
+  return parsedRecipes;
+}
 
 // 2. getOneRecipe(index)
+async function getOneRecipe(index) {
+  const data = await fs.readFile("./recipes-data.json", "utf8");
+  const parsedRecipes = JSON.parse(data);
+
+  return parsedRecipes[index];
+}
+
 
 // 3. getAllRecipeNames()
+async function getAllRecipeNames() {
+    const data = await fs.readFile("./recipes-data.json", "utf8");
+    const parsedRecipes = JSON.parse(data);
+
+    return parsedRecipes.map(recipe => recipe.name);
+}
 
 // 4. getRecipesCount()
+async function getRecipesCount() {
+    const data = await fs.readFile("./recipes-data.json", "utf8");
+    const parsedRecipes = JSON.parse(data);
+
+    return parsedRecipes.length;
+}
 
 // ---------------------------------
 // API Endpoints
 // ---------------------------------
 
 // 1. GET /get-all-recipes
+app.get("/get-all-recipes", async (req, res) => {
+    const recipes = await getAllRecipes();
+
+    res.json(recipes);
+  });
 
 // 2. GET /get-one-recipe/:index
+app.get("/get-one-recipe/:index", async (req, res) => {
+    const index = req.params.index;
+    const recipe = await getOneRecipe(index);
+
+    res.json(recipe);
+  });
 
 // 3. GET /get-all-recipe-names
+app.get("/get-all-recipe-names", async (req, res) => {
+    const recipeNames = await getAllRecipeNames();
+
+    res.json(recipeNames);
+});
 
 // 4. GET /get-recipes-count
+app.get("/get-recipes-count", async (req, res) => {
+    const recipeCount = await getRecipesCount();
+
+    res.json(recipeCount);
+})
